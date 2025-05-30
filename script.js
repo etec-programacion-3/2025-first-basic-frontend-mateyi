f195f0181f307e26c2ba778b713c507faff26173
mateyi <mateogimenez395@gmail.com>
2025-05-30 12:37:57 -0300

    Frontend de biblioteca con búsqueda de libros y README

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function saludar() {
-    alert("¡Hola, mundo desde JavaScript!");
-  }
-  
+function buscarLibro() {
+  const id = document.getElementById('buscador').value;
+  fetch(`http://127.0.0.1:8000/libros/${id}`)
+    .then(res => {
+      if (!res.ok) throw new Error("Libro no encontrado");
+      return res.json();
+    })
+    .then(data => {
+      document.getElementById('resultado').innerHTML = `
+        <h2>${data.titulo}</h2>
+        <p><strong>Autor:</strong> ${data.autor}</p>
+        <p><strong>ID:</strong> ${data.id}</p>
+      `;
+    })
+    .catch(err => {
+      document.getElementById('resultado').innerText = err.message;
+    });
+}
